48ff7795faeefd841a6b779f0735225ab97fe15d
Polecat25 <rokenbu@gmail.com>
2023-05-15 23:31:28 -0600

    owo2

--- src/routes/mg/roles.route.ts
+++ src/routes/mg/roles.route.ts
@@ -6,10 +6,7 @@ import { VerificarRol } from "../../middlewares/role.middleware"
 const routerR = Router()
 
 routerR.get('/roles',VerificarSesion,  VerificarRol(['ADMIN']), GetTodoRol) //traer todos
-//ruta que no necesita autoriazacion 
 routerR.get('/roles/:codigo', GetUnRol)
-
-routerR.get('/owo/', GetUnRol)
 routerR.post('/roles',VerificarSesion, VerificarRol(['ADMIN']), CrearRol) //nuevo
 routerR.put('/roles/:id',VerificarSesion, VerificarRol(['ADMIN']), ModificarRol)// modificar
 routerR.delete('/roles/:id', VerificarSesion, VerificarRol(['ADMIN']),  BorrarRol) //borrar
